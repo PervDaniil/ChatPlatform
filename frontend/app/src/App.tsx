82421f947e5880b6e916f435e57fb601bc47bc82
PervDaniil <studentdan123.900@gmail.com>
2025-01-09 19:57:15 +0600

    Frontend App.js DOM router implemented

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import ChatPage from './pages/ChatPage.tsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-function App() {
-  return (
-    <div className="App">
-      Test
-    </div>
-  );
+export default function App() {
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<ChatPage />} />
+			</Routes>
+		</Router>
+	)
 }
-
-export default App;
