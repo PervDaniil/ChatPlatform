c7bff9d69073ec79cfb3bd7d266341abf2e1f99c
PervDaniil <studentdan123.900@gmail.com>
2025-01-10 18:13:58 +0600

    Theme provider completed

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ChatPage from './pages/ChatPage.tsx';
+import ThemeProvider from './providers/ThemeProvider.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 export default function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<ChatPage />} />
-			</Routes>
-		</Router>
+		<ThemeProvider>
+			<Router>
+				<Routes>
+					<Route path='/' element={<ChatPage />} />
+				</Routes>
+			</Router>
+		</ThemeProvider>
 	)
 }
