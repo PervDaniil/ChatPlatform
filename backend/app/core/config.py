d16f34a237e3f39439427350cc2e7e7c6c3d9e88
PervDaniil <studentdan123.900@gmail.com>
2025-01-12 13:57:21 +0600

    CORS configuration in settings.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,6 @@ BASE_DIR_PATH = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o^v$e+!37v8*m+tcr$*64v30wo1!7j+u+ldbx@=lxbxqn$1mik'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000/',
-    'http://localhost:3000/',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -46,6 +47,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    *config.CORS_ALLOWED_ORIGINS
 ]
 
 
