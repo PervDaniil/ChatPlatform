0de6fb875d2f63d7b264435ee395d576a9aa0079
PervDaniil <studentdan123.900@gmail.com>
2025-01-12 21:43:56 +0600

    API for JWT authentication

--- backend/app/core/urls.py
+++ backend/app/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.users.urls import jwt_urlpatterns
 
 
 urlpatterns = [
+    path('api/v1/token/', include(jwt_urlpatterns)),
     path('admin/', admin.site.urls),
 ]
