6c0ab02979c2367875ceb82e316aa2f52fb1583c
PervDaniil <studentdan123.900@gmail.com>
2025-01-13 23:26:17 +0600

    Chat & Message models + remigration for all models

--- backend/app/api/chats/models.py
+++ backend/app/api/chats/models.py
@@ -1,3 +1,30 @@
+from api.users.models import CustomUser
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    id = models.BigAutoField(primary_key = True)
+    name = models.CharField(max_length=128, blank=False)
+    private = models.BooleanField(default = False)
+    members = models.ManyToManyField(CustomUser)
+    
+    @property
+    def amount_of_members(self):
+        return self.members.count()
+    
+    def __str__(self):
+        return f'{self.id} : {self.name}'
+
+
+class Message(models.Model):
+    id = models.BigAutoField(primary_key = True)
+    text = models.TextField(blank=False)
+    time = models.DateTimeField(auto_now_add=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Message in {self.chat.name} by {self.sender.username}'
+
+
+    
--- backend/app/api/users/models.py
+++ backend/app/api/users/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Profile(models.Model):
+    image = models.ImageField(upload_to='profile/avatars/', blank=True, null=True)
+    
+
 class CustomUser(AbstractUser):
-    id = models.BigAutoField(primary_key = True)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, null=True, blank=True)
     
     def __str__(self):
         return f'{self.id} : {self.username} {self.email}'
