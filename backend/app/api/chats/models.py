a1bce040706df431c9073c45465e29c0f73b4c1d
PervDaniil <studentdan123.900@gmail.com>
2025-01-13 23:35:17 +0600

    Property for CustomUser model

--- backend/app/api/chats/models.py
+++ backend/app/api/chats/models.py
@@ -1,4 +1,3 @@
-from api.users.models import CustomUser
 from django.db import models
 
 
@@ -6,7 +5,7 @@ class Chat(models.Model):
     id = models.BigAutoField(primary_key = True)
     name = models.CharField(max_length=128, blank=False)
     private = models.BooleanField(default = False)
-    members = models.ManyToManyField(CustomUser)
+    members = models.ManyToManyField('users.CustomUser')
     
     @property
     def amount_of_members(self):
@@ -21,7 +20,7 @@ class Message(models.Model):
     text = models.TextField(blank=False)
     time = models.DateTimeField(auto_now_add=True)
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    sender = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
     
     def __str__(self):
         return f'Message in {self.chat.name} by {self.sender.username}'
--- backend/app/api/users/models.py
+++ backend/app/api/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from api.chats.models import Chat
 from django.contrib.auth.models import AbstractUser
 
 
@@ -8,6 +9,10 @@ class Profile(models.Model):
 
 class CustomUser(AbstractUser):
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    
+    @property
+    def chats(self):
+        return Chat.objects.filter(members=self)
     
     def __str__(self):
         return f'{self.id} : {self.username} {self.email}'
