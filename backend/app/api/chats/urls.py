71077db262dbb097437e4042396821738c12811b
PervDaniil <studentdan123.900@gmail.com>
2025-01-14 15:03:32 +0600

    SearchChatView & ChatListViewSet

--- backend/app/api/chats/urls.py
+++ backend/app/api/chats/urls.py
@@ -1,7 +1,8 @@
-from .views import ChatsListViewSet
+from .views import ChatsListViewSet, SearchChatView
 from django.urls import path
 
 
 chats_urlpatterns = [
-    path('', ChatsListViewSet.as_view())
+    path('', ChatsListViewSet.as_view()),
+    path('search/', SearchChatView.as_view()),
 ]
--- backend/app/api/chats/views.py
+++ backend/app/api/chats/views.py
@@ -1,8 +1,18 @@
 from rest_framework.generics import ListAPIView
 from .serializers import ChatModelSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from api.chats.models import Chat
 
 
 class ChatsListViewSet(ListAPIView):
-    queryset = Chat.objects.all()
+    queryset = Chat.objects.filter(private=False)
     serializer_class = ChatModelSerializer
+    
+    
+class SearchChatView(APIView):
+    def get(self, request):
+        search_param = request.query_params.get('name')
+        queryset = Chat.objects.filter(name__icontains=search_param)
+        serializer = ChatModelSerializer(queryset, many=True)
+        return Response(serializer.data)
