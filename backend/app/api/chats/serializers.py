f1d25e044296d002d4c6fa67f6db6a7a0ceaab6f
PervDaniil <studentdan123.900@gmail.com>
2025-01-15 17:24:57 +0600

    ChatMessagesSerializer & Corrections in ChatView

--- backend/app/api/chats/serializers.py
+++ backend/app/api/chats/serializers.py
@@ -1,10 +1,17 @@
 from api.users.serializers import CustomUserModelSerializer
 from rest_framework.serializers import ModelSerializer
-from api.chats.models import Chat
+from api.chats.models import Chat, Message
+
+
+class ChatMessagesSerializer(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = "__all__"
 
 
 class ChatModelSerializer(ModelSerializer):
     members = CustomUserModelSerializer(many=True)
+    messages = ChatMessagesSerializer(many=True)
     
     class Meta:
         model = Chat
